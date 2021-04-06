a4731de243c6bccbc9bff01ab6b7c3bc535e5ee2
daveg7lee <daveg7lee@gmail.com>
2021-04-06 17:01:17 +0900

    Apollo Client

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { useReactiveVar } from '@apollo/client';
+import { ApolloProvider, useReactiveVar } from '@apollo/client';
 import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { isLoggedInVar } from './apollo';
+import { client, isLoggedInVar } from './apollo';
 import routes from './routes';
 import Home from './screens/Home';
 import LogIn from './screens/LogIn';
@@ -11,23 +11,25 @@ import SignUp from './screens/SignUp';
 function App() {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
   return (
-    <HelmetProvider>
-      <Router>
-        <Switch>
-          <Route path={routes.home} exact>
-            {isLoggedIn ? <Home /> : <LogIn />}
-          </Route>
-          {!isLoggedIn && (
-            <Route path={routes.signUp}>
-              <SignUp />
+    <ApolloProvider client={client}>
+      <HelmetProvider>
+        <Router>
+          <Switch>
+            <Route path={routes.home} exact>
+              {isLoggedIn ? <Home /> : <LogIn />}
             </Route>
-          )}
-          <Route>
-            <NotFound />
-          </Route>
-        </Switch>
-      </Router>
-    </HelmetProvider>
+            {!isLoggedIn && (
+              <Route path={routes.signUp}>
+                <SignUp />
+              </Route>
+            )}
+            <Route>
+              <NotFound />
+            </Route>
+          </Switch>
+        </Router>
+      </HelmetProvider>
+    </ApolloProvider>
   );
 }
 
