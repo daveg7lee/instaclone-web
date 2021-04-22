621f704a33dcb82fa2f5f43239f356ee7b03d910
Dave <daveg7lee@gmail.com>
2021-04-22 20:52:54 +0900

    Parsing Hashtags part Two

--- src/components/Feed/Comment.tsx
+++ src/components/Feed/Comment.tsx
@@ -1,4 +1,5 @@
-import sanitizeHtml from 'sanitize-html';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 interface CommentProps {
   author: string | undefined;
@@ -6,24 +7,38 @@ interface CommentProps {
 }
 
 const Comment = ({ author, payload }: CommentProps) => {
-  let cleanedPayload: string = '';
-  if (payload) {
-    cleanedPayload = sanitizeHtml(
-      payload.replace(/#[\w]+/g, '<mark>$&</mark>'),
-      {
-        allowedTags: ['mark'],
-      }
-    );
-  }
   return (
     <div>
       <span className="fat-text">{author}</span>
-      <span
-        className="ml-3"
-        dangerouslySetInnerHTML={{
-          __html: cleanedPayload,
-        }}
-      />
+      <span className="ml-3">
+        {payload?.split(' ').map((word, index) =>
+          /#[\w]+/.test(word) ? (
+            <React.Fragment key={index}>
+              <Link
+                to={`/hashtags/${word}`}
+                className="text-accent hover:underline cursor-pointer font-normal"
+              >
+                {word}
+              </Link>{' '}
+            </React.Fragment>
+          ) : (
+            <React.Fragment key={index}>
+              {/@[\w]+/.test(word) ? (
+                <>
+                  <Link
+                    to={`/user/${word}`}
+                    className="text-accent hover:underline cursor-pointer font-normal"
+                  >
+                    {word}
+                  </Link>{' '}
+                </>
+              ) : (
+                <>{word} </>
+              )}
+            </React.Fragment>
+          )
+        )}
+      </span>
     </div>
   );
 };
