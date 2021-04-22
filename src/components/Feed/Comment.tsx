e95233839bf22f1ba101dc1367c2c5122168fca3
Dave <daveg7lee@gmail.com>
2021-04-22 20:23:36 +0900

    Parsing Hashtags

--- src/components/Feed/Comment.tsx
+++ src/components/Feed/Comment.tsx
@@ -1,13 +1,31 @@
+import sanitizeHtml from 'sanitize-html';
+
 interface CommentProps {
   author: string | undefined;
   payload: string | undefined;
 }
 
-const Comment = ({ author, payload }: CommentProps) => (
-  <div>
-    <span className="fat-text">{author}</span>
-    <span className="ml-3">{payload}</span>
-  </div>
-);
+const Comment = ({ author, payload }: CommentProps) => {
+  let cleanedPayload: string = '';
+  if (payload) {
+    cleanedPayload = sanitizeHtml(
+      payload.replace(/#[\w]+/g, '<mark>$&</mark>'),
+      {
+        allowedTags: ['mark'],
+      }
+    );
+  }
+  return (
+    <div>
+      <span className="fat-text">{author}</span>
+      <span
+        className="ml-3"
+        dangerouslySetInnerHTML={{
+          __html: cleanedPayload,
+        }}
+      />
+    </div>
+  );
+};
 
 export default Comment;
